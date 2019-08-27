307da887732c6cf880d9cbfe5b1f7b082bef22d1
LucasF <lucas.ferrari@corebiz.com.br>
2019-08-27 08:04:35 -0300

    First learn typescript exemple

---
+++ typescript/app2.ts
@@ -0,0 +1,9 @@
+let mensagem: string = "Seja bem vindo !";
+let temporadaFriends: number = 10;
+let estudandoAngular: boolean = true;
+
+let listaDeFrutas: Array <string> = ['pera','uva','maça','salada mista'];
+let listaDeFrutinhas: string [] = ['framboesa', 'cereja'];
+
+let notasDasProvas: Array <number> = [7.5, 8 ,9];
+let notasDosTrabalhos: number[] = [10,9,8];
---
+++ typescript/dist/app2.js
@@ -0,0 +1,8 @@
+"use strict";
+var mensagem = "Seja bem vindo !";
+var temporadaFriends = 10;
+var estudandoAngular = true;
+var listaDeFrutas = ['pera', 'uva', 'maça', 'salada mista'];
+var listaDeFrutinhas = ['framboesa', 'cereja'];
+var notasDasProvas = [7.5, 8, 9];
+var notasDosTrabalhos = [10, 9, 8];
